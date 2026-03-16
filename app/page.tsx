212126edbcf53e8e2d125d759ee90751bdf0e049
AdamKuzniarski <adam.kuzniarski@gmx.de>
2026-03-16 16:05:14 +0100

    add container.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -17,7 +17,7 @@ export default function HomePage() {
           Ein reduzierter Blog mit focus auf Typografie
         </p>
       </header>
-      // test comment
+      // test commen2t
       <section className={'grid gap-4 sm:grid-cols-2'}>
         {links.map((link) => (
           <Link
