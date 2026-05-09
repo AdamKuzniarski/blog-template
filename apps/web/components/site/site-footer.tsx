f858d05cbf51390cab69ce96f7ae3079478dcde3
AdamKuzniarski <adam.kuzniarski@gmx.de>
2026-05-09 14:29:38 +0200

    fix typos"

--- apps/web/components/site/site-footer.tsx
+++ apps/web/components/site/site-footer.tsx
@@ -1,5 +1,5 @@
 import Link from 'next/link';
-import { Container } from '../container';
+import { Container } from '@/components/primitives/container';
 
 export function SiteFooter() {
   return (
